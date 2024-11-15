af9dce71f125f3dc7c97ad50d2d1bac7bd7e2c0f
izzuddinathar <izzuddin.athar@gmail.com>
2024-11-15 13:41:52 +0700

    Folder Structure

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', ['nama' => 'Izzudin Athar']);
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
