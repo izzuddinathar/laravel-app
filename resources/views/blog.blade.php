be08854d8bcd9d706dea439f693b2409f089962b
izzuddinathar <izzuddin.athar@gmail.com>
2024-11-16 11:24:21 +0700

    View Data

--- resources/views/blog.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-    <x-slot:title>{{ $title }}</x-slot:title>
-    <h3 class="text-xl">Welome to my Blog!</h3>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,8 +22,48 @@ Route::get('/about', function () {
     return view('about', ['title' => 'About','name' => 'Izzudin Athar']);
 });
 
-Route::get('/blog', function () {
-    return view('blog',['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts',['title' => 'Blog', 'posts' => [
+        [
+            'id' => 1 ,
+            'slug' => 'judul-artikel-1' ,
+            'title' => 'Judul Artikel 1' ,
+            'author' => 'Izzudin Athar',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptatibus ipsa ea iste voluptate quam architecto fugit sequi quo facilis molestiae in, aperiam molestias dolorum? Ipsa, quaerat in. Dolor, eligendi.',
+        ],
+        [
+            'id' => 2 ,
+            'slug' => 'judul-artikel-2' ,
+            'title' => 'Judul Artikel 2',
+            'author' => 'Izzudin Athar',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt expedita fuga quidem itaque est blanditiis amet temporibus dolorum veniam. Atque recusandae mollitia quia reiciendis aut ipsa, pariatur facilis sit quod!',
+        ]
+    ]]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1 ,
+            'slug' => 'judul-artikel-1' ,
+            'title' => 'Judul Artikel 1' ,
+            'author' => 'Izzudin Athar',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptatibus ipsa ea iste voluptate quam architecto fugit sequi quo facilis molestiae in, aperiam molestias dolorum? Ipsa, quaerat in. Dolor, eligendi.',
+        ],
+        [
+            'id' => 2 ,
+            'slug' => 'judul-artikel-2' ,
+            'title' => 'Judul Artikel 2',
+            'author' => 'Izzudin Athar',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt expedita fuga quidem itaque est blanditiis amet temporibus dolorum veniam. Atque recusandae mollitia quia reiciendis aut ipsa, pariatur facilis sit quod!',
+        ]
+    ];
+
+    $post = Arr::first($posts, function($post) use ($slug){
+        return $post['slug'] == $slug;
+    });
+
+    return view('post',['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
