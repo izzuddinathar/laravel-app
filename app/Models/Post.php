b7f5196257a8ec161723f608d437a8902ac691b3
izzuddinathar <izzuddin.athar@gmail.com>
2024-11-16 13:43:03 +0700

    Eloquent ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,39 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Izzudin Athar',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptatibus ipsa ea iste voluptate quam architecto fugit sequi quo facilis molestiae in, aperiam molestias dolorum? Ipsa, quaerat in. Dolor, eligendi.',
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Izzudin Athar',
-                'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt expedita fuga quidem itaque est blanditiis amet temporibus dolorum veniam. Atque recusandae mollitia quia reiciendis aut ipsa, pariatur facilis sit quod!',
-            ],
-        ];
-    }
+    use HasFactory;
 
-    public static function find($slug): array
-    {
-        $post = Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
-
-        if (! $post) {
-            abort(404);
-        }
-
-        return $post;
-
-    }
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
