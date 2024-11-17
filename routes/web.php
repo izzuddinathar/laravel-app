fd546788a511814d4752e580552688db8f51df99
izzuddinathar <izzuddin.athar@gmail.com>
2024-11-17 20:48:34 +0700

    N + 1 Problem

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,9 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+    // $post = Post::with(['author','category'])->latest()->get();
+    $post = Post::latest()->get();
+    return view('posts', ['title' => 'Blog', 'posts' => $post]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
@@ -33,11 +35,13 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 });
 
 Route::get('/authors/{user:username}', function (User $user) {
-    return view('posts', ['title' => count($user->posts).' Articles By '.$user->name, 'posts' => $user->posts]);
+    $post = $user->posts->load('category', 'author');
+    return view('posts', ['title' => count($user->posts).' Articles By '.$user->name, 'posts' => $post]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts', ['title' => ' Articles in '.$category->name, 'posts' => $category->posts]);
+    $post = $category->posts->load('category', 'author');
+    return view('posts', ['title' => ' Articles in '.$category->name, 'posts' => $post]);
 });
 
 Route::get('/contact', function () {
